f934170c26d9b6b6748d15580dfed7616ff43417
mann620 <nanasicat0620@gmail.com>
2026-05-22 21:55:48 +0900

    a

--- security/permission.py
+++ security/permission.py
@@ -1,7 +1,37 @@
+# =====================================
 # 権限確認
+#
+# login_user_id
+# ↓
+# ログイン中ユーザー
+#
+# file_user_id
+# ↓
+# ファイル所有者
+# =====================================
 def check_permission(
+
     login_user_id,
+
     file_user_id
+
 ):
 
-    return login_user_id == file_user_id
+    # 同じユーザーならOK
+    if (
+        login_user_id
+        ==
+        file_user_id
+    ):
+
+        return True
+
+
+    # 違うなら拒否
+    return False
+result = check_permission(
+    1,
+    2
+)
+
+print(result)
