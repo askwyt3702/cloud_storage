597c385c31a8bbdd5a42cd4c64c203744b6b89b6
askwyt3702 <nekomofu222222@gmail.com>
2026-05-26 13:28:24 +0900

    hennkou

--- backend/main.py
+++ backend/main.py
@@ -16,6 +16,10 @@ from backend.routes.download import (
     router as download_router
 )
 
+from backend.routes.user import (
+    router as user_router
+)
+
 # アップロードAPI  ← 担当A実装後に有効化
 # from backend.routes.upload import (
 #     router as upload_router
@@ -64,6 +68,10 @@ app.include_router(
 # ダウンロード・削除機能追加  ← 担当B追加
 app.include_router(
     download_router
+)
+
+app.include_router(
+    user_router
 )
 
 # アップロード機能追加  ← 担当A実装後に有効化
