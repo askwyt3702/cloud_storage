6235782932020212b9aa17692e17d49d66db8d6c
askwyt3702 <nekomofu222222@gmail.com>
2026-05-26 10:34:57 +0900

    kaeta

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,15 @@
 # FastAPI起動
 from fastapi import FastAPI
 
+# ログインAPI
+from backend.routes.login import (
+    router as login_router
+)
 
-# API読み込み
-from backend.routes.login import router as login_router
-
-from backend.routes.storage import router as storage_router
+# 容量API
+from backend.routes.storage import (
+    router as storage_router
+)
 
 
 # アプリ作成
@@ -13,16 +17,36 @@ app = FastAPI()
 
 
 # ==========================
-# ログイン機能追加
+# トップ画面
 # ==========================
+@app.get("/")
+def home():
+
+    return """
+    <h1>クラウドストレージ</h1>
+
+    <input placeholder='ユーザー名'>
+
+    <br><br>
+
+    <input
+    type='password'
+    placeholder='パスワード'>
+
+    <br><br>
+
+    <button>
+    ログイン
+    </button>
+    """
+
+
+# ログイン機能追加
 app.include_router(
     login_router
 )
 
-
-# ==========================
 # 容量機能追加
-# ==========================
 app.include_router(
     storage_router
 )
