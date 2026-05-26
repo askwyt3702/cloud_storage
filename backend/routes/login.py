0aea2054a134118b20fd380a0e96dc70021682cb
askwyt3702 <nekomofu222222@gmail.com>
2026-05-26 12:59:44 +0900

    変更

--- backend/routes/login.py
+++ backend/routes/login.py
@@ -1,17 +1,11 @@
 from fastapi import APIRouter
-
-# 認証サービス読み込み
 from backend.services.auth_service import (
     login_user
 )
 
-
 router = APIRouter()
 
 
-# =====================================
-# ログインAPI
-# =====================================
 @router.post("/login")
 def login(
 
@@ -21,43 +15,30 @@ def login(
 
 ):
 
-    # 認証実行
     result = login_user(
         username,
         password
     )
 
 
-    # 成功
     if result:
 
         return {
 
             "success": True,
 
+            # ←追加
+            "user": username,
+
             "message": "ログイン成功"
 
         }
 
 
-    # 失敗
     return {
 
         "success": False,
 
-        "message": "ユーザー名またはパスワードが違います"
-
-    }
-
-
-# =====================================
-# ログアウト
-# =====================================
-@router.post("/logout")
-def logout():
-
-    return {
-
-        "success": True
+        "message": "失敗"
 
     }
